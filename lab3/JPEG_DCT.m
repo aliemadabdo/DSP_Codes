c4a031fd79fc4c4e9dafafb6917e0d20a989431b
aliemadabdo <a.e.a3426@gmail.com>
2022-12-20 21:43:32 +0200

    split image function implementation

--- lab3/JPEG_DCT.m
+++ lab3/JPEG_DCT.m
@@ -1,6 +1,8 @@
 clear
 close all
 C8
+
+%==================Main===========================
 %%
 %Block divide
 
@@ -15,22 +17,21 @@ paddedImg=zeros(paddedRows ,paddedCols);
 paddedImg(1:rows,1:cols)= grayImg;      %Divisible by 8 image with zero padding
 
 % ---->>>>>reshape fn needs low level implementation <<<<<---------
-block8by8 = reshape(paddedImg,N,N,[]);  %The 8x8 blocks of the image 
+block8by8 = split_image(paddedImg,[N N]);  %The 8x8 blocks of the image 
 %block8by8(5,5,1450)                    %An example for the block 1450 fifth row fifth column
 
 [x ,y ,numberOfBlocks]=size(block8by8);
-
 DctOfTheBlock =zeros(N,N,numberOfBlocks); %could be discarded and use the varaible "block8by8" directly
 
 %loop on all blocks to apply DCT with C8 on them                                          
 for i=1:numberOfBlocks
-    DctOfTheBlock(:,:,i)= C_8*block8by8(:,:,i)*(C_8.'); %A^=CN*A*CN(transpose)
+    DctOfTheBlock(:,:,i)= C_8.*block8by8(:,:,i).*(C_8.'); %A^=CN*A*CN(transpose)
 end
 disp(DctOfTheBlock);
 %%
 %Quantization
 %A Standard Quantization Matrix for 50% quality 
-r=input("Enter Scaling factor  ") %used  to change quantization level
+%r=input('Enter Scaling factor  '); %used  to change quantization level
 q_mtx =     [16 11 10 16 24 40 51 61; 
             12 12 14 19 26 58 60 55;
             14 13 16 24 40 57 69 56; 
@@ -40,21 +41,24 @@ q_mtx =     [16 11 10 16 24 40 51 61;
             49 64 78 87 103 121 120 101;
             72 92 95 98 112 100 103 99];
 
-q = rescale(q_mtx,r,numberOfBlocks,DctOfTheBlock)
+q = rescale(q_mtx,r,numberOfBlocks,DctOfTheBlock);
 
 %%
 %Rescaling 
-R = rescaling(q,r,q_mtx,numberOfBlocks)
+R = rescaling(q,r,q_mtx,numberOfBlocks);
 
 %%
 %IDCT
 
 %%
 %Merging 
+%%
+
+%===========================end of the main===========================================
 
 %%functions
 function quantized_dct = rescale(x,y,n,dct)
-    T=y.*x
+    T=y.*x;
     for k=1:n
         quantized_dct(:,:,k)= round(dct(:,:,k) ./ T); 
     end
@@ -69,3 +73,26 @@ function rescaled_dct = rescaling(y,r,q,n)
   disp(rescaled_block8by8);
 end
 
+function blocks = split_image(I, block_size)
+%splits the image matrix I into small blocks of size BLOCK_SIZE, and returns the blocks as a 3D matrix.
+
+[nrows, ncols] = size(I);       % Get the number of rows and columns in the image
+
+% Get the number of blocks in each direction
+nblocks_row = ceil(nrows/block_size(1));
+nblocks_col = ceil(ncols/block_size(2));
+
+% Initialize the output matrix
+blocks = zeros(block_size(1), block_size(2), nblocks_row*nblocks_col);
+
+% Split the image into blocks
+for i = 1:nblocks_row
+    for j = 1:nblocks_col
+        % Get the current block
+        block = I((i-1)*block_size(1)+1:i*block_size(1), (j-1)*block_size(2)+1:j*block_size(2));
+        
+        % Store the block in the output matrix
+        blocks(:,:,(i-1)*nblocks_col+j) = block;
+    end
+end
+end
